05b60095c7a4ef9b739585d4583927ed662acfef
Gabri <gabrodflo@alum.us.es>
2019-11-05 18:03:46 +0100

    Linking between pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,9 +1,13 @@
+import { Link } from 'gatsby';
 import React from 'react';
 
 export default ({ children }) => (
   <div>
     <header>
       <h2>Gatsby rocks!</h2>
+      <Link to="/">Home</Link>
+      <Link to="/content">Content</Link>
+      <Link to="/about">About</Link>
     </header>
     <main>{children}</main>
   </div>
