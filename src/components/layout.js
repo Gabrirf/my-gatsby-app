5d800d8baa891b7daaad357742f6ff22d865e046
Gabri <gabrodflo@alum.us.es>
2019-11-05 17:56:10 +0100

    Add Nested Layout

---
+++ src/components/layout.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default ({ children }) => (
+  <div>
+    <header>
+      <h2>Gatsby rocks!</h2>
+    </header>
+    <main>{children}</main>
+  </div>
+)
--- src/pages/about.js
+++ src/pages/about.js
@@ -1,4 +1,5 @@
 import React from 'react';
 
+import Layout from '../components/layout';
 
-export default () => <div>This is me!</div>
+export default () => <Layout>This is me!</Layout>
